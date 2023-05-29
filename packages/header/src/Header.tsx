cbc9f5d3cf72c3edd1c3ec717825f6fd97b4c7d3
takeshi <taekshisuz57@gmail.com>
2023-05-28 21:49:39 -0400

    run 2

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! workflow should run 1
+            Lerna is the original monorepo tool! workflow should run 2
         </header>
     );
 }
